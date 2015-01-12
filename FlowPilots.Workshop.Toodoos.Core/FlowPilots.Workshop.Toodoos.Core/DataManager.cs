b755601c219b1d016879d6f25391e43093b952b1
Jan Van de Poel <jan@flowpilots.com>
2015-01-12 18:55:05 +0100

    added a basic PCL Core

---
+++ FlowPilots.Workshop.Toodoos.Core/FlowPilots.Workshop.Toodoos.Core/DataManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FlowPilots.Workshop.Toodoos.Core
+{
+    public class DataManager
+    {
+        public DataManager()
+        {
+        }
+
+        public List<Todo> GetAllTasks()
+        {
+            return new List<Todo>
+            {
+                new Todo
+                {
+                    Done = false,
+                    Name = "Unit test",
+                    Description = "Write a unit test for our PCL"
+                },
+                new Todo
+                {
+                    Done = false,
+                    Name = "iOS app",
+                    Description = "Create an iOS application based on the PCL"
+                },
+                new Todo
+                {
+                    Done = false,
+                    Name = "Android app",
+                    Description = "Create an Android application based on the PCL"
+                }
+            };
+        }
+    }
+}
+
---
+++ FlowPilots.Workshop.Toodoos.Core/FlowPilots.Workshop.Toodoos.Core/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FlowPilots.Workshop.Toodoos.Core
+{
+    public class Todo
+    {
+        public Todo()
+        {
+        }
+
+        public bool Done { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
+
