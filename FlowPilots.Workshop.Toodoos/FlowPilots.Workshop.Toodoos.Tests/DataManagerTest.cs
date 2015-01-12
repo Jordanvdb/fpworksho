23103bfbf54167409582c570501894d16f1e1bb9
Jan Van de Poel <jan@flowpilots.com>
2015-01-12 19:09:20 +0100

    added Arrange, Act, Assert in the test project

---
+++ FlowPilots.Workshop.Toodoos/FlowPilots.Workshop.Toodoos.Tests/DataManagerTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace FlowPilots.Workshop.Toodoos.Tests
+{
+    [TestFixture()]
+    public class DataManagerTest
+    {
+        [Test()]
+        public void TestTodoCount()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
+
